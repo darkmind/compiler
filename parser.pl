df9e797a070ccf0907057b7d12f7a59654006feb
Nikolay Malkov <Nikolay.Malkov@portaone.com>
2016-04-15 17:24:24 +0300

    Push tokens to stack, use current and next tokens
    Ie it will be LL(1) parser

--- parser.pl
+++ parser.pl
@@ -8,10 +8,30 @@ use Data::Dumper;
 
 my $debug = 0;
 
+my @stack;
 while( <> ){
     my $line = $_;
 
     my $json_decoder = JSON->new();
     my $result       = $json_decoder->decode( $line );
-    print Dumper( $result );
+
+    my $token_pos   = 0;
+    foreach my $token ( @{ $result } ){
+        my $curr_token  = $result->[$token_pos];
+        my $next_token  = $result->[$token_pos + 1];
+
+        push( @stack, $curr_token );
+        check_rules( \@stack, $next_token );
+
+        $token_pos++;
+    }
+}
+
+sub check_rules {
+    my ( $stack, $next_token ) = @_;
+
+    print Dumper( $stack );
+    print "Next token: " . Dumper( $next_token );
+
+    return ;
 }
